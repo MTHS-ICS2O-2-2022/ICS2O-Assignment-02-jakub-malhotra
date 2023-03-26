87644296ffeab6550fcd4339ac3975e0e24d30a2
jakub-malhotra <124389450+jakub-malhotra@users.noreply.github.com>
2023-03-26 00:15:53 +0000

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Jakub Malhotra
+// Created on: March 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+/**
+ * This function calculates the area of a parallelogram
+ */
+function enterClicked() {
+  //input
+  const base = parseInt(document.getElementById("base-of-parallelogram").value)
+  const height = parseInt(
+    document.getElementById("height-of-parallelogram").value
+  )
+
+  //process
+  const area = base * height
+
+  //output
+  document.getElementById("area-of-parallelogram").innerHTML =
+    "The area of the parallelogram is " + area + " cm²."
+}
